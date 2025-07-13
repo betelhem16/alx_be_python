9f1907947e71a7ba543064e84d0c62ecb9d14ac2
betelhem16 <betelhem.kassaye@aastustudent.edu.et>
2025-07-13 10:02:21 +0300

    Add basic_operations.py script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to number1 and number2
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print results in the required format
+print("Addition of", number1 ,"and", number2 , "is" , addition)
+print("Subtraction of " ,number1,  "and", number2 , "is", subtraction)
+print("Multiplication of",  number1 , "and" ,number2, "is", multiplication)
